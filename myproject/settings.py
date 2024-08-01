0ede7fd7694a6883c1c11c87e1e89e2107df136f
tmdalsdl <lsm123452002@gmail.com>
2024-08-01 12:11:04 +0900

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'OPTIONS': {
             'charset': 'utf8mb4',
             'use_unicode': True,
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'init_command': "SET NAMES 'utf8mb4' COLLATE 'utf8mb4_unicode_ci'",
         },
     }
 }
